329d4ff1c853494caa2da6c51e00708476effe2b
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2022-05-21 19:16:40 -0700

    add snippet1 test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,6 +8,19 @@ import java.util.List;
 
 public class MarkdownParseTest {
 
+     @Test
+    public void snippet1 () throws IOException {
+        ArrayList<String> forTest1 = new ArrayList<String>();
+        forTest1.add("url.com");
+        forTest1.add("`google.com");
+        forTest1.add("google.com");
+        forTest1.add("ucsd.edu");
+
+        Path fileName = Path.of("snippet1.md");
+        String content = Files.readString(fileName);
+        assertEquals(forTest1, MarkdownParse.getLinks(content));
+    }
+    
     @Test
     public void testFile1() throws IOException {
         String contents= Files.readString(Path.of("./test-file.md"));
